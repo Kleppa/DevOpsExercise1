e3957292e3cdd51b8c9a1f66e0a3b84deff47c94
Kleppa <jarandwaagekleppa@gmail.com>
2018-09-03 16:01:28 +0200

    init

---
+++ excersise1/ci/mvn-build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+ls -all
+mvn -f git-repo/
--- excersise1/src/test/java/com/klejar/ex1/AppTest.java
+++ excersise1/src/test/java/com/klejar/ex1/AppTest.java
@@ -1,4 +1,16 @@
 package com.klejar.ex1;
 
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 public class AppTest {
+	@Test
+	public void testAdd(){
+		int sum = 1+2;
+		assertEquals(sum,3);
+	}
+
 }
